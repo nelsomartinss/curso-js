89f3cdac1f9ceeed871372b311b4e753338e7e02
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2026-02-17 17:26:28 -0300

    comparasion operators

---
+++ aula05.js
@@ -0,0 +1,19 @@
+// Comparasion Operators
+
+// > greater than
+// < less than
+// == equal to
+// >= greather than or equal to
+// <= less than or equal to
+// === strict equality
+// != defferent from
+// !== strict difference
+
+console.log(10 > 5); // 10 is greather than 5
+console.log(5 < 10) // 5 is less than 10
+console.log(5 == 5) // 5 is equal to 5
+console.log(10 >= 10) // 10 is greather than or equal to 10
+console.log(10 <= 9) // 10 is not less than nor equal to 9 
+console.log(10 === '10'); // 10 is not same type as '10'
+console.log(10 != '10'); // 10 is not different from '10' in this case
+console.log(10 !== '10'); // 10 is different from '10' is this case
