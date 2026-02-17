e1009ad6b9ea017d6af3b65d1d82850ceef0799f
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2026-02-17 16:16:02 -0300

    assignment operators

---
+++ aula04.js
@@ -0,0 +1,22 @@
+// Assignment Operators
+
+// = simple assignment
+// += combined addition
+// -= combined subtraction
+// *= combined multiplication
+// /= combined division
+// %= combined rest
+
+let number = 24; // simple assignment
+console.log(number);
+
+number += 24; // combined addition
+console.log(number);
+number -= 2; // combined subtraction
+console.log(number);
+number /= 3; // combined division
+console.log(number);
+number *= 10; // combined multiplication
+console.log(number);
+number %= 2; // combined rest
+console.log(number);
