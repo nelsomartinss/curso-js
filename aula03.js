1d539bb2a006eb69f05f3fedcaf97a736cadf5c6
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2026-02-17 15:50:35 -0300

    Arithmetic Operators

---
+++ aula03.js
@@ -0,0 +1,9 @@
+// Arithmetic Operators
+
+const sum = 4000 + 7000 + 250 + 200;
+const sub = 20990 - 11000;
+const div = sub / 24;
+const multi = div * 24;
+const rest = div % 2; 
+
+console.log(sum, sub, div, multi, rest)
