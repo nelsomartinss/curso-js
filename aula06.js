adc97473ca74ddbb734759df2b0e6388a3dcb072
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2026-02-18 10:39:39 -0300

    logic operators

---
+++ aula06.js
@@ -0,0 +1,16 @@
+// Logic Operators
+
+/*
+    && / and
+    || / or
+    ! / (not)
+*/
+
+// && - all values ​​must be true.
+console.log(10 == 10 && 5 == 5);
+
+// || - only one of the values must be true.
+console.log(10 == 10 || 10 < 5);
+
+// inverts the value
+console.log(10 > 1); // invert the value to false or true
